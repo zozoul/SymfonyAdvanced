b6ab0fa382962fd7c3204348ee4df1d8f8e8ce41
Herizo Rasolomampionona <hrz.rasolo@gmail.com>
2021-07-13 23:14:13 +0300

    2021-07-13 [EVOL] (Zo) Premiere requete

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Classe permettant de gérer l'affichage de la page racine
+ * 
+ */
 class HomeController extends AbstractController {
     /**
      * @Route("/", name="home")
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,10 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class PropertyController extends AbstractController {
+
+/**
+ * Classe permettant de gérer la propriété d'une page
+ * 
+ * 
+ * 
+ * @since 2021-07-12 
+ */
+class PropertyController extends AbstractController 
+{
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    
+    /**
+     * Constructeur
+     */
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
 
     /**
      * @Route("/biens", name="property.index")
@@ -14,6 +38,23 @@ class PropertyController extends AbstractController {
      */
     public function index(): Response
     {
+       $property = $this->repository->find(1);
+       dump($property);
+        /*$property = new Property();
+        $property->setTitle('Mon premier bien')
+            ->setPrice(200000)
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('une petite description')
+            ->setSurface(60)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Montpellier')
+            ->setAddress('15 Boulevard Gambetta')
+            ->setPostalCode('34000');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();*/
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
         ]);
